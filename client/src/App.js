3fc6e4312803704490ef9115710d7a2027a22572
Dally Wally <dallin.watac.major@gmail.com>
2018-09-20 23:18:50 -0600

    Made a test card that gets populated with all the plant info

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
 // import Image from './components/Image';
+import PlantCard from './components/PlantCard';
 import API from './utils';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: "dallinmajor"
+            username: "dallinmajor",
+            firstname: '',
+            lastname: '',
+            plants: [],
+            id: '',
+            profile_picture: '',
+            cover_photo: ''
         };
     }
 
     async componentDidMount() {
-        // this.getImages();
         this.getUser()
-    }
-
-    getImages() {
-        API.getUser(this.state.props)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-            // this.setState({ images: res.data })
     }
 
     getUser() {
         API.getUser(this.state.username)
-            .then(res => console.log(res))
+            .then(user => this.setState({
+                firstname: user.data.firstname,
+                lastname: user.data.lastname,
+                plants: user.data.plants,
+                id: user.data._id,
+                profile_picture: user.data.profile_picture,
+                cover_photo: user.data.cover_photo
+            }))
             .catch(err => console.log(err));
     }
 
@@ -32,7 +38,15 @@ class App extends Component {
         return (
             <div>
                 <div className="container">
-
+                    {this.state.plants.map(plant => (
+                        <PlantCard 
+                            key={plant._id}
+                            name={plant.name} 
+                            image={plant.image}
+                            description={plant.description}
+                            comment={plant.comments[0].comment}
+                        /> 
+                    ))}
                 </div>
             </div>
         );
---
+++ client/src/components/PlantCard/plantcard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './plantcard.css';
+
+const PlantCard = props => (
+    <div>
+        <div className="plantCard card">
+            <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
+            </div>
+            <img className="cardImage card-img-bottom" src={'/api/images/' + props.image} alt={props.name}/>
+            <div className="card-body">
+                <p className="card-text">{props.description}</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                <br></br>
+                <p>{props.comment}</p>
+            </div>
+        </div>
+    </div>
+);
+
+export default PlantCard;
