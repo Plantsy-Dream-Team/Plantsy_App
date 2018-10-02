a959e5249ae062a0e3f57fe27d103701c7860987
Robidobi33 <robidobi33@gmail.com>
2018-10-01 19:31:07 -0600

    html folder

---
+++ client/src/components/PicCard/PicCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./PicCard.css";
+
+const PicCard = props => (
+  <div>
+     <div class = "proPic">
+        <div className="img-container">
+            <img alt={props.name} src={props.image} />
+        </div>
+        <div class = "proName">{props.name}</div>
+    </div>
+  </div>
+);
+
+export default PicCard;
