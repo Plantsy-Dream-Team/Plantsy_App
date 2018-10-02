ac55436409d77ba3792e47f5367b8b1a79770b6e
Robidobi33 <robidobi33@gmail.com>
2018-10-01 20:18:20 -0600

    added pic components

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,16 +1,15 @@
 import React from "react";
 
 const Jumbotron = ({ children }) => (
-  <div class = "jumbo">  {children}
-        <div class = "proPic">
-            <div class = "proName">Profile Name</div>
-        </div>
-        <div class = "title">Plantsy
-            <input class = "btnL" type = "submit" value = "Logout"/> 
-            <input class = "btnL" type = "submit" value = "Search"/>
-            <input class = "btnL" type = "submit" value = "Profile"/>
-            <input class = "btnL" type = "submit" value = "Home"/>
-        </div>
+  <div class = "jumbo">
+      <input class = "btnP" type = "submit" value = "Post a Plant"></input>
+      {children}
+      <div class = "title">Plantsy
+          <input class = "btnL" type = "submit" value = "Logout"/> 
+          <input class = "btnL" type = "submit" value = "Search"/>
+          <input class = "btnL" type = "submit" value = "Profile"/>
+          <input class = "btnL" type = "submit" value = "Home"/>
+      </div>
     </div>
 );
 
