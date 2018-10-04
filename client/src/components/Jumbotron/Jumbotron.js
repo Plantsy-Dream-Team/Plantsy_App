dfa574912da28faa81a58e44d6efac9390aa5a00
Robidobi33 <robidobi33@gmail.com>
2018-10-03 21:02:36 -0600

    updated search button

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -4,9 +4,9 @@ const Jumbotron = ({ children }) => (
   <div class = "jumbo">
       <input class = "btnP" type = "submit" value = "Post a Plant"></input>
       {children}
+      <input class = "btnL" type = "submit" value = "Search"/>
       <div class = "title">Plantsy
           <input class = "btnL" type = "submit" value = "Logout"/> 
-          <input class = "btnL" type = "submit" value = "Search"/>
           <input class = "btnL" type = "submit" value = "Profile"/>
           <input class = "btnL" type = "submit" value = "Home"/>
       </div>
